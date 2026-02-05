db0d7838ec1271d1d8b44bb6dc90941aa9e67b13
Sabinmdhr <Sabinmanandhar22@gmail.com>
2026-02-05 16:09:23 +0545

    loginPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
+import Button from '@mui/material/Button';
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     hello
-    </>
-  )
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+
+
+    </Routes>
+   </BrowserRouter>
+  );
 }
 
 export default App
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Link,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
+import "../assets/scss/pages/login.scss";
+import logo from "../assets/swift-logo.svg";
 
-const Login = () => {
+export default function LoginCard() {
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleClickShowPassword = () => setShowPassword((prev) => !prev);
+  const handleMouseDownPassword = (event) => event.preventDefault();
   return (
-    <div>Login</div>
-  )
+    <div className="loginContainer">
+      <Card className="loginCard">
+        <div className="image">
+          <img src={logo} alt="" />
+        </div>
+        <div className="loginForm">
+          <form action="">
+            <TextField
+              label="Email"
+              className="field"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              className="field"
+              label="Password"
+              type={showPassword ? "password" : "text"}
+              variant="outlined"
+              fullWidth
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+            <div className="actions">
+              <Link href="/forgot-password" className="forgetPassword">
+                Forgot Password
+              </Link>
+              <FormControlLabel
+                className="rememberMe"
+                control={<Checkbox />}
+                label="Remember Me"
+              />
+            </div>
+            <Button type="submit" className="loginButton" variant="contained">
+              Login
+            </Button>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 }
-
-export default Login
