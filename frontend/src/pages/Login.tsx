5cbf61542012cc01812b8316b7c3a1cc8eb1d23e
Sabinmdhr <Sabinmanandhar22@gmail.com>
2026-02-25 10:20:12 +0545

    changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,27 +7,43 @@ import {
   InputAdornment,
   IconButton,
 } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useState } from "react";
 import "../assets/scss/pages/login.scss";
 import logo from "../assets/swift-logo.svg";
 import { Link } from "react-router-dom";
+import { axiosInstance } from "../api/api";
 
 export default function LoginCard() {
   const [showPassword, setShowPassword] = useState(true);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+} = useForm<LoginFormInputs>();
 
-  
-    const onSubmit = (data:any) => {
-      console.log("Valid Data:", data);
-    };
+type LoginFormInputs = {
+  email: string;
+  password: string;
+};
+
+
+   const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
+     try {
+       const response = await axiosInstance.post("api/v1/login", data);
+
+       console.log("Login Success:", response.data);
+       // you can store token
+       localStorage.setItem("token", response.data.token);
+     } catch (error: any) {
+
+       console.error("Login Failed:", error.response?.data);
+       alert("Invalid email or password");
+     }
+   };
   // const handleClickShowPassword = () => setShowPassword((prev) => !prev);
   // const handleMouseDownPassword = (event) => event.preventDefault();
   return (
@@ -38,7 +54,7 @@ export default function LoginCard() {
         <Link to={{pathname:"/"}} className="outlook"><span>Use your outlook acoount</span></Link>
         </div>
         <div className="loginForm">
-          <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <TextField
               label="Email"
               className="field"
@@ -46,10 +62,10 @@ export default function LoginCard() {
               fullWidth
               {...register("email", {
                 required: "Email is required",
-                pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: "Invalid Email",
-                },
+                // pattern: {
+                //   value: /\S+@\S+\.\S+/,
+                //   message: "Invalid Email",
+                // },
               })}
               error={!!errors.email}
               helperText={errors.email?.message as String}
@@ -75,10 +91,10 @@ export default function LoginCard() {
               }}
               {...register("password", {
                 required: "Password is required",
-                minLength: {
-                  value: 6,
-                  message: "Min 6 characters",
-                },
+                // minLength: {
+                //   value: 6,
+                //   message: "Min 6 characters",
+                // },
               })}
               error={!!errors.password}
               helperText={errors.password?.message as String}
